216e26c2daa6d4f97a039759c621f44c952e1365
2aurora2 <1971261337@qq.com>
2024-03-07 11:39:49 +0800

    Fix: 修复初始化项目的Bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,26 +2,11 @@
   <h1>
     首页
   </h1>
-  <button @click="login">login</button>
 </template>
 
 <script setup>
-import { useUserStore } from '@/store/modules/useUserStore.js'
-import { loginService } from '@/api/user.js'
+import {useUserStore} from '@/store/modules/useUserStore.js'
 
 // 先创建实例对象再使用
 const userStore = useUserStore();
-
-const loginData = {
-  username: "2aurora2",
-  password: "123456"
-}
-
-const login = () => {
-  loginService(loginData).then((res) => {
-    console.log(res.data);
-  }).catch((err) => {
-    console.log(err);
-  })
-}
 </script>
