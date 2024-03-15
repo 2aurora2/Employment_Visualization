bfc421fda9fd82013f59399f4cd128ffb9141039
2aurora2 <1971261337@qq.com>
2024-03-15 20:12:00 +0800

    职业薪资水平分析

--- src/views/Recruitment/SalaryAnalysisView.vue
+++ src/views/Recruitment/SalaryAnalysisView.vue
@@ -1,11 +1,176 @@
 <template>
   <div class="container">
-    <div id="main" style="height: 580px;width: 950px"></div>
+    <div class="user-choose">
+      <div class="select">
+        <select id="city-choose" class="form-select" aria-label="Default select example" data-bs-theme="dark"
+                v-model="curBigCate">
+        </select>
+      </div>
+    </div>
+    <div class="salary-bar">
+      <div id="main" style="height: 550px;width: 950px"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import {onMounted, ref, watch} from "vue";
+import * as echarts from "echarts";
+import JobData from '@/assets/data/job_data.json'
 
+const curBigCate = ref('后端开发')
+
+const cateWatch = watch(curBigCate, (newVal, oldVal) => {
+  SalaryNumber()
+})
+
+onMounted(() => {
+  const bigCategory = Object.keys(JobData)
+
+  const dom = document.getElementById("city-choose")
+  bigCategory.forEach(cate => {
+    const option = document.createElement('option');
+    option.text = cate;
+    option.value = cate;
+    dom.add(option);
+  })
+
+  SalaryNumber()
+})
+
+const SalaryNumber = () => {
+  const bigObj = JobData[curBigCate.value]  // 大类对象
+  const smlObj = Object.keys(bigObj)        // 小类名称
+
+  const average = []
+  const midVal = []
+  const xLabel = []
+
+  smlObj.forEach(cate => {
+    xLabel.push(cate);
+    midVal.push(parseInt(bigObj[cate]['中位数'].replace("¥", "").replace(",", "")))
+    average.push(parseInt(bigObj[cate]['平均数'].replace("¥", "").replace(",", "")))
+  })
+
+  const dom = echarts.init(document.getElementById('main'))
+  dom.setOption({
+    darkMode: true,
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        crossStyle: {
+          color: '#999'
+        }
+      }
+    },
+    // 区域缩放
+    dataZoom: [
+      {
+        type: 'slider',
+        show: true,
+        startValue: xLabel[0],
+        endValue: xLabel[xLabel.length > 6 ? 6 : xLabel.length]
+      },
+      {
+        type: 'inside'
+      }
+    ],
+    toolbox: {
+      feature: {
+        // 保存图片
+        saveAsImage: {
+          show: true,
+          title: '保存为图片',
+          type: 'png',
+          excludeComponents: ['toolbox']
+        },
+        // 重新加载
+        restore: {
+          show: true
+        }
+      }
+    },
+    legend: {
+      data: ['月薪资中位数', '月薪资平均数'],
+      textStyle: {
+        color: '#dcdcdc'
+      },
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: xLabel,
+        axisPointer: {
+          type: 'shadow'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: '月薪资中位数',
+        // min: 0,
+        // max: 250,
+        axisLabel: {
+          formatter: '{value} 元'
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            type: 'dashed',
+            color: '#2c2a46'
+          }
+        }
+      },
+      {
+        type: 'value',
+        name: '月薪资平均数',
+        // min: 0,
+        // max: Math.max(average),
+        axisLabel: {
+          formatter: '{value} 元'
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            type: 'dashed',
+            color: '#302e50'
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: '月薪资中位数',
+        type: 'bar',
+        tooltip: {
+          valueFormatter: function (value) {
+            return value + ' 元';
+          }
+        },
+        data: midVal,
+        itemStyle: {
+          borderRadius: [5, 5, 0, 0]
+        },
+      },
+      {
+        name: '月薪资平均数',
+        type: 'bar',
+        yAxisIndex: 1,
+        tooltip: {
+          valueFormatter: function (value) {
+            return value + ' 元';
+          }
+        },
+        data: average,
+        itemStyle: {
+          borderRadius: [5, 5, 0, 0]
+        },
+      }
+    ]
+  })
+}
 </script>
 
 <style scoped lang="scss">
@@ -13,7 +178,23 @@
   width: 1000px;
   height: 600px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  .user-choose {
+    display: flex;
+    height: 50px;
+    width: 100%;
+    margin-top: 26px;
+
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+
+    #city-choose {
+      width: 200px;
+    }
+  }
 }
 </style>
