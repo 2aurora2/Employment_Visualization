8e11247be550d1f2a739fbf5133580d885ad36a6
2aurora2 <1971261337@qq.com>
2024-03-17 01:28:15 +0800

    招聘岗位整体情况分析图

--- src/views/Recruitment/OverallAnalysisView.vue
+++ src/views/Recruitment/OverallAnalysisView.vue
@@ -1,13 +1,202 @@
 <template>
-  <div>
-    123
+  <div class="container">
+    <div class="radio">
+      <div class="form-check form-check-inline" data-bs-theme="dark">
+        <input class="form-check-input" type="radio" name="keyword" id="keyword1" value="0" checked
+               v-model="curType">
+        <label class="form-check-label" for="keyword1" style="color: #dcdcdc">工作所需经验</label>
+      </div>
+      <div class="form-check form-check-inline" data-bs-theme="dark">
+        <input class="form-check-input" type="radio" name="keyword" id="keyword2" value="1" v-model="curType">
+        <label class="form-check-label" for="keyword2" style="color: #dcdcdc">工作性质</label>
+      </div>
+      <div class="form-check form-check-inline" data-bs-theme="dark">
+        <input class="form-check-input" type="radio" name="keyword" id="keyword3" value="2" v-model="curType">
+        <label class="form-check-label" for="keyword3" style="color: #dcdcdc">公司团队规模</label>
+      </div>
+      <div class="form-check form-check-inline" data-bs-theme="dark">
+        <input class="form-check-input" type="radio" name="keyword" id="keyword4" value="3" v-model="curType">
+        <label class="form-check-label" for="keyword4" style="color: #dcdcdc">所需学历</label>
+      </div>
+    </div>
+    <div id="main" style="height: 650px;width: 1000px"></div>
   </div>
 </template>
 
 <script setup>
+import * as echarts from 'echarts'
+import {onMounted, ref, shallowRef, watch} from "vue";
+import RecruitInfoNotProcess from '@/assets/data/招聘信息汇总.json'
+import RecruitInfoAfterProcess from '@/assets/data/招聘信息_afterProcessing.json'
 
+const curType = ref(0)
+const type = ref(['experience', 'attribute', 'nop', 'qualification'])
+const recruitIndoNotProcess = Array.isArray(RecruitInfoNotProcess) ? RecruitInfoNotProcess : []
+const recruitInfoAfterProcess = Array.isArray(RecruitInfoAfterProcess) ? RecruitInfoAfterProcess : []
+const dom = shallowRef()
+
+const typeWatch = watch(curType, (newVal, oldVal) => {
+  const originData = recruitInfoAfterProcess.map(item => [item['site'], item['revenue']])
+  const sites = Array.from(new Set(originData.map(item => item[0])))
+  const mean = []
+  sites.forEach(site => {
+    let filter = originData.filter(item => item[0] === site)
+    const revenue = filter.map(item => item[1])
+    const meanPer = revenue.reduce((total, per) => total + per, 0) / revenue.length
+    mean.push(meanPer)
+  })
+
+  showBar(sites, mean)
+})
+
+onMounted(() => {
+  const originData = recruitInfoAfterProcess.map(item => [item['site'], item['revenue']])
+  const sites = Array.from(new Set(originData.map(item => item[0])))
+  const mean = []
+  sites.forEach(site => {
+    let filter = originData.filter(item => item[0] === site)
+    const revenue = filter.map(item => item[1])
+    const meanPer = revenue.reduce((total, per) => total + per, 0) / revenue.length
+    mean.push(meanPer)
+  })
+
+  dom.value = echarts.init(document.getElementById('main'))
+  showBar(sites, mean)
+})
+
+const showBar = (sites, mean) => {
+  console.log(mean)
+  const colName = type.value[Number(curType.value)]
+  const originData = recruitIndoNotProcess.map(item => [item['site'], item[colName]])
+  const types = Array.from(new Set(originData.map(item => item[1])))
+  const series = []
+  types.forEach(type => {
+    const number = []
+    sites.forEach(site => {
+      number.push(originData.filter(item => item[0] === site && item[1] === type).length)
+    })
+    const per = {
+      data: number,
+      type: 'bar',
+      stack: colName,
+      name: type,
+      yAxisIndex: 0,
+    }
+    series.push(per)
+  })
+  series.push({
+    name: '平均薪资（元）',
+    type: 'line',
+    stack: 'item',
+    data: mean,
+    yAxisIndex: 1
+  })
+  dom.value.setOption({
+        legend: {
+          show: true,
+          textStyle: {
+            color: '#dcdcdc'
+          }
+        },
+        itemStyle: {
+          borderRadius: [3, 3, 0, 0]
+        },
+        xAxis: {
+          type: 'category',
+          data: sites
+        },
+        yAxis: [{
+          type: 'value',
+          name: colName,
+          position: 'left',
+          alignTicks: true,
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#dcdcdc'
+            }
+          },
+          axisLabel: {
+            formatter: '{value}'
+          }
+        },
+          {
+            type: 'value',
+            name: '平均薪资（元）',
+            position: 'right',
+            alignTicks: true,
+            axisLine: {
+              show: true,
+              lineStyle: {
+                color: '#dcdcdc'
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
+            }
+          }
+        ],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            crossStyle: {
+              color: '#999'
+            }
+          }
+        },
+        // 区域缩放
+        dataZoom: [
+          {
+            type: 'slider',
+            show: true,
+            startValue: '北京',
+            endValue: '哈尔滨'
+          },
+          {
+            type: 'inside'
+          }
+        ],
+        toolbox: {
+          feature: {
+            // 保存图片
+            saveAsImage: {
+              show: true,
+              title: '保存为图片',
+              type: 'png',
+              excludeComponents: ['toolbox']
+            },
+            // 重新加载
+            restore: {
+              show: true
+            }
+          }
+        },
+        series: series
+      }
+  )
+}
 </script>
 
 <style scoped lang="scss">
+.container {
+  width: 1000px;
+  height: 650px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  .radio {
+    height: 40px;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+
+    .form-check {
+      color: #dcdcdc;
+      margin-left: 10px;
+    }
+  }
+}
 </style>
