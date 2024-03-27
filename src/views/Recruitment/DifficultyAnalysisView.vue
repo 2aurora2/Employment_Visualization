f940ae1c8f4bc93503531ca5c70e0089e9755536
2aurora2 <1971261337@qq.com>
2024-03-27 14:30:36 +0800

    添加难度系数提示TIPS

--- src/views/Recruitment/DifficultyAnalysisView.vue
+++ src/views/Recruitment/DifficultyAnalysisView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="container">
+    <div class="tips">
+      <div class="tip-icon">
+        <img src="@/assets/image/tip.svg" alt="tip" style="width: 20px;height: auto;cursor: pointer"
+             @mouseover="showTips">
+        <p class="tip-content" id="content">
+          招聘岗位难度系数是由该岗位的：薪资、所需工作经验、所需学历、公司规模、工作性质等属性进行TOPSIS综合评价得到的。
+        </p>
+      </div>
+    </div>
     <div id="main" style="height: 650px;width: 1000px"></div>
   </div>
 </template>
@@ -18,6 +27,17 @@ const cityArray = Array.from(new Set(recruitmentArray.map(item => item.site)));
 
 const diffOfCities = ref({})
 const boxplotEcharts = shallowRef()
+
+const showTips = () => {
+  const dom = document.getElementById('content')
+  dom.style.height = '85px';
+  dom.style.padding = '5px 6px 5px 10px'
+
+  setTimeout(() => {
+    dom.style.height = '0'
+    dom.style.padding = '0'
+  }, 6000)
+}
 
 onBeforeMount(() => {
   cityArray.forEach(city => {
@@ -75,6 +95,7 @@ onMounted(() => {
       }
     ],
     toolbox: {
+      left: 68,
       feature: {
         // 保存图片
         saveAsImage: {
@@ -201,8 +222,40 @@ onMounted(() => {
 .container {
   width: 1000px;
   height: 650px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background: transparent;
   border: none !important;
   box-shadow: none;
+
+  .tips {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding-right: 2px;
+
+    .tip-icon {
+      position: relative;
+
+      .tip-content {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        width: 350px;
+        height: 0;
+        background-color: #3d61d2;
+        padding: 0;
+        border-radius: 5px;
+        color: #FFFFFF;
+        font-size: 15px;
+        font-family: 宋体, serif;
+        transition: height 1s, padding 1s;
+        overflow: hidden;
+      }
+    }
+  }
 }
 </style>
