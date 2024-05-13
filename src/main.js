696671224b8f255aafafefc17bdcc04d910b5a6c
2aurora2 <1971261337@qq.com>
2024-05-13 17:54:38 +0800

    deploy

--- src/main.js
+++ src/main.js
@@ -8,4 +8,5 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+
 createApp(App).use(router).use(store).mount('#app')
